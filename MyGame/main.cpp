928dcc00258d8779ec32fa0e31e41fb91fffad19
Riw Royer <ne.free532@gmail.com>
2020-07-09 21:42:47 +0800

    Добавил экран конца игры. Добавил отображение координат.

--- MyGame/main.cpp
+++ MyGame/main.cpp
@@ -18,11 +18,45 @@ float fPlayerX = 1.0f;
 float fPlayerY = 1.0f;
 float fPlayerXBuf = fPlayerX;
 float fPlayerYBuf = fPlayerY;
+float fEndGameX = 2.0f;										// Координаты конца игры
+float fEndGameY = 2.0f;									
 float fPlayerA = 0.0f;										// Направление игрока
 float fSpeed = 4.0f;										// Скорость передвижения
 
 float fFoV = 3.14159f / 4.0f;								// Угол обзора
 float fDepth = 75.0f;										// Максимальная дистанция обзора
+
+void game_over(wchar_t* console, wchar_t a, wchar_t b, wchar_t c)
+{
+	int16_t rand_flag = rand() % 10;
+
+	for (int16_t x = 0; x < iConsoleWidth; x++)
+	{
+		for (int16_t y = 0; y < iConsoleHeight; y++)
+		{
+			if (rand_flag != 0)
+			{
+				console[y * iConsoleWidth + x] = b;
+			}
+
+			else
+			{
+				console[y * iConsoleWidth + x] = 'G';
+				console[y * iConsoleWidth + x + 1] = 'A';
+				console[y * iConsoleWidth + x + 2] = 'M';
+				console[y * iConsoleWidth + x + 3] = 'E';
+				console[y * iConsoleWidth + x + 4] = ' ';
+				console[y * iConsoleWidth + x + 5] = 'O';
+				console[y * iConsoleWidth + x + 6] = 'V';
+				console[y * iConsoleWidth + x + 7] = 'E';
+				console[y * iConsoleWidth + x + 8] = 'R';
+				y += 8;
+			}
+
+			rand_flag = rand() % 3;
+		}
+	}
+}
 
 int main()
 {
@@ -36,6 +70,7 @@ int main()
 	DWORD dwBytesWritten = 0;
 
 	wstring map;
+	int32_t iStopwatch = 0;
 
 	map += L"##################################################";
 	map += L"#................................#...............#";
@@ -173,172 +208,185 @@ int main()
 	// Игровой цикл
 	while (true)
 	{
-		aTimePoint2 = chrono::system_clock::now();
-		chrono::duration<float> elapsedTime = aTimePoint2 - aTimePoint1;
-		aTimePoint1 = aTimePoint2;
-		float fElapsedTime = elapsedTime.count();
 
-		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)		// Клавишей "A" поворачиваем по часовой стрелке
-			fPlayerA -= (fSpeed * 0.5f) * fElapsedTime;
-
-		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)		// Клавишей "D" поворачиваем против часовой стрелки
-			fPlayerA += (fSpeed * 0.5f) * fElapsedTime;
-
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)		// Клавишей "W" идём вперёд
+		if ((fPlayerX >= fEndGameX - 1.0f) && (fPlayerX <= fEndGameX + 1.0f) && (fPlayerY >= fEndGameY - 1.0f) && (fPlayerY <= fEndGameY + 1.0f))
 		{
-			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;
-			fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;
-
-			if (map[(int)fPlayerY * iMapWidth + (int)fPlayerX] == '#') { // Если столкнулись со стеной, то откатываем шаг
-				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
-				fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
-			}
+			game_over(console, 0x2591, 0x256C, 0x253C);
 		}
 
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)		// Клавишей "S" идём назад
+		else
 		{
-			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
-			fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
-			if (map[(int)fPlayerY * iMapWidth + (int)fPlayerX] == '#') { // Если столкнулись со стеной, но откатываем шаг
+			iStopwatch = clock();
+
+			aTimePoint2 = chrono::system_clock::now();
+			chrono::duration<float> elapsedTime = aTimePoint2 - aTimePoint1;
+			aTimePoint1 = aTimePoint2;
+			float fElapsedTime = elapsedTime.count();
+
+			if (GetAsyncKeyState((unsigned short)'A') & 0x8000)		// Клавишей "A" поворачиваем по часовой стрелке
+				fPlayerA -= (fSpeed * 0.5f) * fElapsedTime;
+
+			if (GetAsyncKeyState((unsigned short)'D') & 0x8000)		// Клавишей "D" поворачиваем против часовой стрелки
+				fPlayerA += (fSpeed * 0.5f) * fElapsedTime;
+
+			if (GetAsyncKeyState((unsigned short)'W') & 0x8000)		// Клавишей "W" идём вперёд
+			{
 				fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;
 				fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;
+
+				if (map[(int)fPlayerY * iMapWidth + (int)fPlayerX] == '#') { // Если столкнулись со стеной, то откатываем шаг
+					fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
+					fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
+				}
 			}
-		}
 
-		for (int16_t x = 0; x < iConsoleWidth; x++)
-		{
-			// Находим расстояние до стенки в направлении fRayAngle 
-			float fRayAngle = (fPlayerA - fFoV / 2.0f) + ((float)x / (float)iConsoleWidth) * fFoV;	// Направление луча
-			float fDistanceToWall = 0.0f;															// Дистанция до стены
-			bool bHitWall = false;																	// Достигнул ли луч стенку
-			bool bBoundary = false;																	// Достигнул ли луч границу между стенами
-
-			float fEyeX = sinf(fRayAngle);													// Координаты единичного вектора
-			float fEyeY = cosf(fRayAngle);
-
-			while (!bHitWall && fDistanceToWall < fDepth)		// Пока мы не столкнулись со стеной и дистанция до стены меньше максимальная дистанция обзора
+			if (GetAsyncKeyState((unsigned short)'S') & 0x8000)		// Клавишей "S" идём назад
 			{
-				fDistanceToWall += 0.1f;
-
-				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);								// Точка на игровом поле
-				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);								// в которую попал луч
-
-				if (nTestX < 0 || nTestX >= iMapWidth || nTestY < 0 || nTestY >= iMapHeight)
-				{																					// Если мы вышли за зону
-					bHitWall = true;
-					fDistanceToWall = fDepth;
+				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;
+				fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;
+				if (map[(int)fPlayerY * iMapWidth + (int)fPlayerX] == '#') { // Если столкнулись со стеной, но откатываем шаг
+					fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;
+					fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;
 				}
+			}
 
-				else if (map[nTestY * iMapWidth + nTestX] == '#')	// Проверяем, не является ли ячейка луча стеной
+			for (int16_t x = 0; x < iConsoleWidth; x++)
+			{
+				// Находим расстояние до стенки в направлении fRayAngle 
+				float fRayAngle = (fPlayerA - fFoV / 2.0f) + ((float)x / (float)iConsoleWidth) * fFoV;	// Направление луча
+				float fDistanceToWall = 0.0f;															// Дистанция до стены
+				bool bHitWall = false;																	// Достигнул ли луч стенку
+				bool bBoundary = false;																	// Достигнул ли луч границу между стенами
+
+				float fEyeX = sinf(fRayAngle);													// Координаты единичного вектора
+				float fEyeY = cosf(fRayAngle);
+
+				while (!bHitWall && fDistanceToWall < fDepth)		// Пока мы не столкнулись со стеной и дистанция до стены меньше максимальная дистанция обзора
 				{
-					bHitWall = true;
-					//--
-					vector <pair <float, float>> p;
+					fDistanceToWall += 0.1f;
 
-					for (int tx = 0; tx < 2; tx++)
-						for (int ty = 0; ty < 2; ty++)						// Проходим по всем 4м рёбрам
-						{
-							float vx = (float)nTestX + tx - fPlayerX;			// Координаты вектора,
-							float vy = (float)nTestY + ty - fPlayerY;			// ведущего из наблюдателя в ребро
-							float d = sqrt(vx * vx + vy * vy);					// Модуль этого вектора
-							float dot = (fEyeX * vx / d) + (fEyeY * vy / d);	// Скалярное произведение (единичных векторов)
-							p.push_back(make_pair(d, dot));						// Сохраняем результат в массив
-						}
-					// Мы будем выводить два ближайших ребра, поэтому сортируем их по модулю вектора ребра
-					sort(p.begin(), p.end(), [](const pair <float, float>& left, const pair <float, float>& right) {return left.first < right.first; });
+					int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);								// Точка на игровом поле
+					int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);								// в которую попал луч
 
-					float fBound = 0.005;							// Угол, при котором начинаем различать ребро.
-					if (acos(p.at(0).second) < fBound) bBoundary = true;
-					if (acos(p.at(1).second) < fBound) bBoundary = true;
-					//--
-				}
+					if (nTestX < 0 || nTestX >= iMapWidth || nTestY < 0 || nTestY >= iMapHeight)
+					{																					// Если мы вышли за зону
+						bHitWall = true;
+						fDistanceToWall = fDepth;
+					}
 
-				//Вычисляем дистанцию потолка и пола
-				int16_t iCeiling = (float)(iConsoleHeight / 2.0) - iConsoleWidth / ((float)fDistanceToWall);
-				int16_t iFloor = iConsoleHeight - iCeiling;
-
-				short siShade;
-
-				if (fDistanceToWall <= fDepth / 4.0f)        siShade = 0x2588; // Если стенка близко, то рисуем 
-				else if (fDistanceToWall < fDepth / 3.0f)    siShade = 0x2593; // светлую полоску
-				else if (fDistanceToWall < fDepth / 2.0f)    siShade = 0x2592; // Для отдалённых участков 
-				else if (fDistanceToWall < fDepth)           siShade = 0x2591; // рисуем более темную
-				else                                         siShade = ' ';
-
-				if (bBoundary)		siShade = 0x2551; // 2502
-
-				for (int y = 0; y < iConsoleHeight; y++)
-				{
-					if (y <= iCeiling)
-						console[y * iConsoleWidth + x] = ' ';
-					else if (y > iCeiling && y <= iFloor)
-						console[y * iConsoleWidth + x] = siShade;
-					else
+					else if (map[nTestY * iMapWidth + nTestX] == '#')	// Проверяем, не является ли ячейка луча стеной
 					{
-						// То же самое с полом - более близкие части рисуем более заметными символами
-						float b = 1.0f - ((float)y - iConsoleHeight / 2.0) / ((float)iConsoleHeight / 2.0);
-						if (b < 0.25)        siShade = '#';
-						else if (b < 0.5)    siShade = 'x';
-						else if (b < 0.75)   siShade = '.';
-						else if (b < 0.9)    siShade = '-';
-						else                 siShade = ' ';
+						bHitWall = true;
+						//--
+						vector <pair <float, float>> p;
 
-						console[y * iConsoleWidth + x] = siShade;
+						for (int tx = 0; tx < 2; tx++)
+							for (int ty = 0; ty < 2; ty++)						// Проходим по всем 4м рёбрам
+							{
+								float vx = (float)nTestX + tx - fPlayerX;			// Координаты вектора,
+								float vy = (float)nTestY + ty - fPlayerY;			// ведущего из наблюдателя в ребро
+								float d = sqrt(vx * vx + vy * vy);					// Модуль этого вектора
+								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);	// Скалярное произведение (единичных векторов)
+								p.push_back(make_pair(d, dot));						// Сохраняем результат в массив
+							}
+						// Мы будем выводить два ближайших ребра, поэтому сортируем их по модулю вектора ребра
+						sort(p.begin(), p.end(), [](const pair <float, float>& left, const pair <float, float>& right) {return left.first < right.first; });
+
+						float fBound = 0.005;							// Угол, при котором начинаем различать ребро.
+						if (acos(p.at(0).second) < fBound) bBoundary = true;
+						if (acos(p.at(1).second) < fBound) bBoundary = true;
+						//--
+					}
+
+					//Вычисляем дистанцию потолка и пола
+					int16_t iCeiling = (float)(iConsoleHeight / 2.0) - iConsoleWidth / ((float)fDistanceToWall);
+					int16_t iFloor = iConsoleHeight - iCeiling;
+
+					short siShade;
+
+					if (fDistanceToWall <= fDepth / 4.0f)        siShade = 0x2588; // Если стенка близко, то рисуем 
+					else if (fDistanceToWall < fDepth / 3.0f)    siShade = 0x2593; // светлую полоску
+					else if (fDistanceToWall < fDepth / 2.0f)    siShade = 0x2592; // Для отдалённых участков 
+					else if (fDistanceToWall < fDepth)           siShade = 0x2591; // рисуем более темную
+					else                                         siShade = ' ';
+
+					if (bBoundary)		siShade = 0x2551; // 2502
+
+					for (int y = 0; y < iConsoleHeight; y++)
+					{
+						if (y <= iCeiling)
+							console[y * iConsoleWidth + x] = ' ';
+						else if (y > iCeiling && y <= iFloor)
+							console[y * iConsoleWidth + x] = siShade;
+						else
+						{
+							// То же самое с полом - более близкие части рисуем более заметными символами
+							float b = 1.0f - ((float)y - iConsoleHeight / 2.0) / ((float)iConsoleHeight / 2.0);
+							if (b < 0.25)        siShade = '#';
+							else if (b < 0.5)    siShade = 'x';
+							else if (b < 0.75)   siShade = '.';
+							else if (b < 0.9)    siShade = '-';
+							else                 siShade = ' ';
+
+							console[y * iConsoleWidth + x] = siShade;
+						}
 					}
 				}
 			}
-		}
 
-		// Звук шагов с небольшой задержкой
-		if ((fPlayerXBuf != fPlayerX || fPlayerYBuf != fPlayerY) && iWalkDelay == 0)
-		{
-			fPlayerXBuf = fPlayerX;
-			fPlayerYBuf = fPlayerY;
-			s1->play();														// Проигрываем звук шагов
-			s1->setVolume(0.5f);
-		}
-
-		else if (fPlayerXBuf == fPlayerX && fPlayerYBuf == fPlayerY)
-		{
-			s1->stop();
-			iWalkDelay = 0;
-		}
-
-		else if (iWalkDelay == 30)
-		{
-			iWalkDelay = -1;
-		}
-
-		iWalkDelay++;
-
-		if (iSoundEffectDelay == 0)
-		{
-			int16_t q = rand() % 3;
-
-			if (q == 0)
+			// Звук шагов с небольшой задержкой
+			if ((fPlayerXBuf != fPlayerX || fPlayerYBuf != fPlayerY) && iWalkDelay == 0)
 			{
-				s2->play();														// Проигрываем шепот
-				s2->setVolume(0.4f);
-				iSoundEffectDelay = 5000 + rand() % 1000;
+				fPlayerXBuf = fPlayerX;
+				fPlayerYBuf = fPlayerY;
+				s1->play();														// Проигрываем звук шагов
+				s1->setVolume(0.5f);
 			}
 
-			else if (q == 1)
+			else if (fPlayerXBuf == fPlayerX && fPlayerYBuf == fPlayerY)
 			{
-				s3->play();														// Проигрываем зловещие звуки
-				s3->setVolume(0.6f);
-				iSoundEffectDelay = 5000 + rand() % 1000;
+				s1->stop();
+				iWalkDelay = 0;
 			}
 
-			else if (q == 2)
+			else if (iWalkDelay == 30)
 			{
-				s4->play();														// Проигрываем голос
-				s4->setVolume(0.6f);
-				iSoundEffectDelay = 5000 + rand() % 1000;
+				iWalkDelay = -1;
 			}
+
+			iWalkDelay++;
+
+			if (iSoundEffectDelay == 0)
+			{
+				int16_t q = rand() % 3;
+
+				if (q == 0)
+				{
+					s2->play();														// Проигрываем шепот
+					s2->setVolume(0.4f);
+					iSoundEffectDelay = 5000 + rand() % 1000;
+				}
+
+				else if (q == 1)
+				{
+					s3->play();														// Проигрываем зловещие звуки
+					s3->setVolume(0.6f);
+					iSoundEffectDelay = 5000 + rand() % 1000;
+				}
+
+				else if (q == 2)
+				{
+					s4->play();														// Проигрываем голос
+					s4->setVolume(0.6f);
+					iSoundEffectDelay = 5000 + rand() % 1000;
+				}
+			}
+
+			iSoundEffectDelay--;
+
+			// Вывод координат и таймера
+			swprintf_s(console, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f, Time: %4.3f", fPlayerX, fPlayerY, fPlayerA, (float)iStopwatch/1000.0f);
 		}
-
-		iSoundEffectDelay--;
-
 		//for (int16_t nx = 0; nx < iMapWidth; nx++)
 		//	for (int16_t ny = 0; ny < iMapHeight; ny++)
 		//	{
