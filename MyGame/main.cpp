4c95209ec351cb91cebf6cd41ef30c942cf633c4
Riw Royer <ne.free532@gmail.com>
2020-07-14 22:52:35 +0800

    Добавлена beta карты + исправлена количество строк в файле ob2

--- MyGame/main.cpp
+++ MyGame/main.cpp
@@ -2,8 +2,8 @@
 
 int16_t iConsoleWidth = 150;							// Ширина консоли
 int16_t iConsoleHeight = 40;							// Высота консоли
-int16_t iMapWidth = 100;								// Ширина карты
-int16_t iMapHeight = 50;								// Высота карты
+int16_t iMapWidth = 200;								// Ширина карты
+int16_t iMapHeight = 100;								// Высота карты
 
 float fPlayerX = 1.0f;
 float fPlayerY = 1.0f;
